0f925742f7a5ed420e6913025d94c1abf2e868f5
Makoto Nishimura <mac.nishimura@gmail.com>
2012-10-04 00:51:50 +0900

    first commit

---
+++ UnderlineSample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnderlineSample.Common;
+
+namespace UnderlineSample.ViewModels
+{
+    class MainPageViewModel : BindableBase
+    {
+        /// <summary>
+        /// アンダーラインを引きたい場合こちらに値を入れる
+        /// </summary>
+        private string _underlineText;
+
+        public string underlineText
+        {
+            get { return _underlineText; }
+            set
+            {
+                _underlineText = value;
+                OnPropertyChanged("underlineText");
+            }
+        }
+
+        /// <summary>
+        /// アンダーラインを引きたくない場合はこちらに値を入れる
+        /// </summary>
+        private string _normalText;
+
+        public string normalText
+        {
+            get { return _normalText; }
+            set
+            {
+                _normalText = value;
+                OnPropertyChanged("normalText");
+            }
+        }
+    }
+}
